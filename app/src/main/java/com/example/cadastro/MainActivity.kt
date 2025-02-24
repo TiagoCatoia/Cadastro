d6fd9660033792ac5893a5c36eaeb1c2176b4e86
Tiago Catoia <tiagocsz@hotmail.com>
2025-02-24 18:23:48 -0300

    feat: add layout components

--- app/src/main/java/com/example/cadastro/MainActivity.kt
+++ app/src/main/java/com/example/cadastro/MainActivity.kt
@@ -5,10 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.cadastro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val activityMainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        with (activityMainBinding) {
+
+        }
     }
+
 }
